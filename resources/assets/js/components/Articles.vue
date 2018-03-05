245566f550c289831a73d81474fdba13612e1a3c
unknown <rahmani.majda@gmail.com>
2018-03-05 11:11:52 +0000

    modifier fichier js

--- resources/assets/js/components/Articles.vue
+++ resources/assets/js/components/Articles.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
       <h2>Articles</h2>
+      <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid ab inventore assumenda cumque. Voluptates dolor enim saepe corporis placeat, eaque ab at cupiditate, id error expedita, maxime voluptatibus porro incidunt.</p>
   </div>
 </template> 
